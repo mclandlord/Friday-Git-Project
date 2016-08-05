14b14d808b2b956af528f6d7f6804cbbaf0d4df3
Gordon Kung <gordon.kung212@gmail.com>
2016-08-05 16:03:49 -0400

    car

---
+++ Car.h
@@ -0,0 +1,17 @@
+//
+//  Car.h
+//  Friday Git Project
+//
+//  Created by Gordon Kung on 8/5/16.
+//  Copyright © 2016 TurnToTech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Car : NSObject
+
+@property (nonatomic, strong) NSString *car;
+@property (nonatomic, strong) NSString *wheels;
+
+
+@end
---
+++ Car.m
@@ -0,0 +1,14 @@
+//
+//  Car.m
+//  Friday Git Project
+//
+//  Created by Gordon Kung on 8/5/16.
+//  Copyright © 2016 TurnToTech. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+
+@end
