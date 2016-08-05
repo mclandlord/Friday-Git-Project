f76d656cdc16bf82dd07179c97e080c91ca16da4
Clyff Millet <clyffm@gmail.com>
2016-08-05 15:56:12 -0400

    fdas

---
+++ clyffsViewController.h
@@ -0,0 +1,13 @@
+//
+//  clyffsViewController.h
+//  Friday Git Project
+//
+//  Created by Clyfford Millet on 8/5/16.
+//  Copyright © 2016 TurnToTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface clyffsViewController : UIViewController
+
+@end
---
+++ clyffsViewController.m
@@ -0,0 +1,38 @@
+//
+//  clyffsViewController.m
+//  Friday Git Project
+//
+//  Created by Clyfford Millet on 8/5/16.
+//  Copyright © 2016 TurnToTech. All rights reserved.
+//
+
+#import "clyffsViewController.h"
+
+@interface clyffsViewController ()
+
+@end
+
+@implementation clyffsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"hi");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
