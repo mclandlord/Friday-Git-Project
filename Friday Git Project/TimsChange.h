b5d76bf0b71c2173f2a257117b3ad838d4b90872
timothyjhull <timothyjhull@gmail.com>
2016-08-05 15:55:59 -0400

    tims commit

---
+++ Friday Git Project/TimsChange.h
@@ -0,0 +1,13 @@
+//
+//  TimsChange.h
+//  Friday Git Project
+//
+//  Created by Timothy Hull on 8/5/16.
+//  Copyright © 2016 TurnToTech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TimsChange : NSObject
+
+@end
---
+++ Friday Git Project/TimsChange.m
@@ -0,0 +1,13 @@
+//
+//  TimsChange.m
+//  Friday Git Project
+//
+//  Created by Timothy Hull on 8/5/16.
+//  Copyright © 2016 TurnToTech. All rights reserved.
+//
+
+#import "TimsChange.h"
+//yolo
+@implementation TimsChange
+
+@end
