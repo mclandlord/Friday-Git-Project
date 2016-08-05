3656a5008a4ed89ac69ae1fdcdfbdd5dd25ed454
mclandlord <marc@marccianciola.com>
2016-08-05 15:56:25 -0400

    Time file added

---
+++ Friday Git Project/Timer.h
@@ -0,0 +1,13 @@
+//
+//  Timer.h
+//  Friday Git Project
+//
+//  Created by Aditya Narayan on 8/5/16.
+//  Copyright © 2016 TurnToTech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Timer : NSObject
+
+@end
