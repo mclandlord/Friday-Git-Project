3403897cd3a72545f5ca21211232821cdbf657d2
mclandlord <marc@marccianciola.com>
2016-08-05 16:04:32 -0400

    cocoa puffs

---
+++ Friday Git Project/CocoaPuffs.h
@@ -0,0 +1,13 @@
+//
+//  CocoaPuffs.h
+//  Friday Git Project
+//
+//  Created by Aditya Narayan on 8/5/16.
+//  Copyright © 2016 TurnToTech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CocoaPuffs : NSObject
+
+@end
---
+++ Friday Git Project/CocoaPuffs.m
@@ -0,0 +1,13 @@
+//
+//  CocoaPuffs.m
+//  Friday Git Project
+//
+//  Created by Aditya Narayan on 8/5/16.
+//  Copyright © 2016 TurnToTech. All rights reserved.
+//
+
+#import "CocoaPuffs.h"
+
+@implementation CocoaPuffs
+
+@end
